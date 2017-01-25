8cca3a257e50ba8ad333820a5ad75f7335f73582
lizhihui0215 <lizhihui0215@gmail.com>
2017-01-25 10:24:41 +0800

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Article} from "./article/article";
 
 @Component({
   selector: 'app-root',
@@ -7,18 +6,7 @@ import {Article} from "./article/article";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  articles: Article[];
-
-
   constructor() {
-    this.articles = [];
-  }
-
-  addArticle (title: HTMLInputElement, link: HTMLInputElement): void {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    this.articles.push(new Article(title.value, link.value, 0));
-    title.value = '';
-    link.value = '';
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent
   ],
   imports: [
     BrowserModule,
